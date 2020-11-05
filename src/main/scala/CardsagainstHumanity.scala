7884d07acdd5eb577908b87a5edcb51cfb51cfbe
exportPipe <paulruess97@gmail.com>
2020-11-05 16:36:05 +0100

    added project structure with view, model and controller

--- src/main/scala/CardsagainstHumanity.scala
+++ src/main/scala/CardsagainstHumanity.scala
@@ -1,5 +1,16 @@
+import model.{BlackCards, Player, WhiteCards}
+
 object CardsagainstHumanity {
   def main(args: Array[String]): Unit = {
     println("Cards against Humanity! by Niklas and Paul")
+
+    val player1 = Player("Niklas")
+    val testBC = BlackCards("___ ist sehr cool")
+    val testWC = WhiteCards("Barack Obama")
+
+    println(player1.toString)
+    println(testBC.toString)
+    println(testWC.toString)
+
   }
 }
