432d424fc712d24672bdec95fb9947ad11b351a4
exportPipe <paulruess97@gmail.com>
2021-01-07 00:39:45 +0100

    added GUI and major bug fixes

--- src/main/scala/aview/A_StartView.scala
+++ src/main/scala/aview/A_StartView.scala
@@ -5,6 +5,8 @@ import java.awt.{Color, Toolkit}
 import scala.swing._
 import scala.swing.{BorderPanel, Button, ComboBox, Dimension, FlowPanel}
 import control.ControllerInterface
+import javax.swing.ImageIcon
+
 import scala.swing.event.ButtonClicked
 
 class A_StartView(infotextbar: Infotextbar, controller: ControllerInterface) extends BorderPanel {
@@ -25,16 +27,27 @@ class A_StartView(infotextbar: Infotextbar, controller: ControllerInterface) ext
     contents += new FlowPanel() {
       background = Color.WHITE
       foreground = Color.BLACK
-      preferredSize = new Dimension(300, 150)
+      preferredSize = new Dimension(1000, 60)
       contents += nrOfPlrCB
       contents += lbl1
       contents += startButton
     }
   }
 
+  def titlePanel: FlowPanel = new FlowPanel() {
+    background = Color.BLACK
+    preferredSize = new Dimension(1000, 400)
+    private val titlePic = new ImageIcon("src/main/scala/aview/Z_Cards_Against_Humanity_logo.png").getImage
+    private val titlePic500 = titlePic.getScaledInstance(300, 300, java.awt.Image.SCALE_SMOOTH)
+    contents += new Label {
+      icon = new ImageIcon(titlePic500)
+    }
+  }
+
   add(mainPanel, BorderPanel.Position.South)
+  add(titlePanel, BorderPanel.Position.North)
   infotextbar.text = "select number of player"
-  preferredSize = new Dimension(200, 400)
+  preferredSize = new Dimension(1000, 400)
 
   listenTo(startButton)
 
